ba933d9f8afc1aa1f16431aed3ed2df1545779dc
Ari Putra <ariteknologi@gmail.com>
2019-01-27 02:07:34 +0700

    add base navbar layout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -14,4 +14,15 @@
             <span>Sifat Tanah</span>
         </a>
     </li>
+    <li class="nav-item with-sub">
+        <a class="nav-link" href="#">
+            <span>Administrator</span>
+        </a>
+        <div class="sub-item">
+            <ul>
+                <li><a href="page-profile.html">List Pengguna</a></li>
+                <li><a href="page-invoice.html">List Level Pengguna</a></li>
+            </ul>
+        </div>
+    </li>
 </ul>
