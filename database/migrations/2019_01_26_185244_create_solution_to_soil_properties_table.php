c6622a0ef869393d1de5190b72dcfc825f130b40
Ari Putra <ariteknologi@gmail.com>
2019-01-27 01:56:45 +0700

    add base migration

---
+++ database/migrations/2019_01_26_185244_create_solution_to_soil_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolutionToSoilPropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solution_to_soil_properties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('soil_properties_id');
+            $table->timestamps();
+
+            $table->foreign('soil_properties_id')->references('id')->on('soil_properties')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solution_to_soil_properties');
+    }
+}
