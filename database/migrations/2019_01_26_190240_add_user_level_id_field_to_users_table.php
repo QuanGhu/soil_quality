6342de831c7bda93c147570c60cefe358218e737
Ari Putra <ariteknologi@gmail.com>
2019-01-27 02:03:54 +0700

    add additional migration

---
+++ database/migrations/2019_01_26_190240_add_user_level_id_field_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserLevelIdFieldToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('user_level_id');
+            $table->foreign('user_level_id')->references('id')->on('user_level')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
