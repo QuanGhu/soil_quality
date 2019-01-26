354eff26075f40b93f48aac9d56df1fa7a09318c
Ari Putra <ariteknologi@gmail.com>
2019-01-27 02:31:31 +0700

    add datatables and form collective package

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Collective\Html\HtmlServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
     ],
 
     /*
@@ -223,7 +224,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Crud' => App\Helpers\CrudHelpers::class
+        'Crud' => App\Helpers\CrudHelpers::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
