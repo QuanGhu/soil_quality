3ae1ec01589b6f1ac9cbc584b0c542458c162e48
Ari Putra <ariteknologi@gmail.com>
2019-01-31 22:12:30 +0700

    change user dashboard design

--- resources/views/home/userdashboard.blade.php
+++ resources/views/home/userdashboard.blade.php
@@ -1,38 +1,33 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="card card-table">
-            <div class="card-header">
-                <h6 class="slim-card-title">Hasil Analisa Terakhir</h6>
-            </div><!-- card-header -->
-            <div class="table-responsive">
-                <table class="table mg-b-0 tx-13">
-                    <thead>
-                        <tr class="tx-10">
-                            <th class="wd-10p pd-y-5">No</th>
-                            <th class="pd-y-5">Tanggal</th>
-                            <th class="pd-y-5">Kemungkinan Sifat Tanah</th>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h4>Hasil Analisa Terakhir</h4>
+        </div>
+        <div class="col-md-12">
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="tx-10">
+                        <th class="wd-10p pd-y-5">No</th>
+                        <th class="pd-y-5">Tanggal</th>
+                        <th class="pd-y-5">Kemungkinan Sifat Tanah</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($anaylises as $key => $value)
+                        <tr>
+                            <td>{{ $key+1 }}</td>
+                            <td>{{ $value->created_at }}</td>
+                            <td>{{ $value->result }}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @forelse($anaylises as $key => $value)
-                            <tr>
-                                <td>{{ $key+1 }}</td>
-                                <td>{{ $value->created_at }}</td>
-                                <td>{{ $value->result }}</td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="3">
-                                    Anda Belum Melakukan Analisa
-                                </td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-            </div><!-- table-responsive -->
-            <div class="card-footer tx-12 pd-y-15 bg-transparent">
-                <a href="{{ route('customer.index') }}"><i class="fa fa-angle-down mg-r-5"></i>Lihat Semua Data</a>
-            </div><!-- card-footer -->
+                    @empty
+                        <tr>
+                            <td colspan="3">
+                                Anda Belum Melakukan Analisa
+                            </td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
